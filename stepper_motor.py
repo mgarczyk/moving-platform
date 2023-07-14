9204f9bcf907a9fd7f220ae3a657c1eb2d2ca75d
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-07-14 14:22:42 +0200

    Stepper motor testing script.

--- stepper_motor.py
+++ stepper_motor.py
@@ -1,17 +1,28 @@
 from periphery import GPIO
 import time
 
-STEPPER_PIN=73
-STEPPER_GPIO=GPIO(STEPPER_PIN,"out")
-while True:
-    try:
-        STEPPER_GPIO.write(True)
-        time.sleep(0.001)
-        STEPPER_GPIO.write(False)
-        time.sleep(0.001)
-    except KeyboardInterrupt:
-        STEPPER_GPIO.write(False)
-        break
-    except IOError:
-        print("IOErr")
-        break
+Step_Pin=73
+Dir_Pin=74
+Step_GPIO=GPIO(Step_Pin,"out")
+Dir_GPIO=GPIO(Dir_Pin,"out")
+Direction=True
+def Stepper_motor(Direction):
+    Dir_GPIO.write(Direction)
+    for y in range (10):
+        for x in range (200):
+            try:
+                Step_GPIO.write(True)
+                time.sleep(0.005)
+                Step_GPIO.write(False)
+                time.sleep(0.005)
+            except KeyboardInterrupt:
+                Step_GPIO.write(False)
+                break
+            except IOError:
+                print("IOErr")
+                break
+def main():
+    Stepper_motor(1) #1 for CW - clockwise 0 for counterclockwise
+
+if __name__ == __main__:
+    main()
