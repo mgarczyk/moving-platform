633472a90d1b718663b43c7d70eb9408469469e9
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-07-20 10:09:13 +0200

    Local mqtt server

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -2,13 +2,11 @@ from paho.mqtt import client as mqtt_client
 import random
 import time
 
-broker = '192.168.1.103'
+broker = 'localhost'
 port = 1883
-topic = "python/mqtt"
+topic = "/home/radxa/mqtt"
 # Generate a Client ID with the publish prefix.
 client_id = f'publish-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -18,11 +16,9 @@ def connect_mqtt():
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
-
 
 def publish(client):
     msg_count = 1
@@ -39,7 +35,6 @@ def publish(client):
         msg_count += 1
         if msg_count > 20:
             break
-
 
 def run():
     client = connect_mqtt()
--- mqtt_sub.py
+++ mqtt_sub.py
@@ -1,13 +1,10 @@
 import random
-
 from paho.mqtt import client as mqtt_client
 
-
-broker = '192.168.1.103'
+broker = 'localhost'
 port = 1883
 topic = "/home/radxa/mqtt"
 client_id = f'subscribe-{random.randint(0, 100)}'
-
 
 
 def connect_mqtt() -> mqtt_client:
@@ -16,9 +13,7 @@ def connect_mqtt() -> mqtt_client:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
@@ -27,7 +22,6 @@ def connect_mqtt() -> mqtt_client:
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
     client.subscribe(topic)
     client.on_message = on_message
 
