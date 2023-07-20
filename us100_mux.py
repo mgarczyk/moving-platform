de185e9f5db231627416350c1221f3bc6cf16a8f
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-07-20 12:21:09 +0200

    Delete usless stuff.

--- us100_mux.py
+++ us100_mux.py
@@ -1,20 +1,14 @@
-from re import U
 from periphery import GPIO
 import time
 import adafruit_us100
 import time
 import serial
-import os
-import subprocess
 
 ##MUX PINOUT##
 MUX_PIN_A=71
 MUX_PIN_B=72
 MUX_GPIO_A=GPIO(MUX_PIN_A,"out")
 MUX_GPIO_B=GPIO(MUX_PIN_B,"out")
-
-##UART ENABLE / US100
-os.system("systemctl stop serial-getty@ttyS2.service")
 
 def Sensors(VAR_A,VAR_B):     
     UART_US100=serial.Serial("/dev/ttyS4",baudrate=9600,timeout=1)
