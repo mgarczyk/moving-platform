c3479ace52802aaa926915055b2efa23f15c5a5d
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-07-20 12:15:43 +0200

    Delete stepper motor part that will be diff script

--- us100_mux.py
+++ us100_mux.py
@@ -13,21 +13,8 @@ MUX_PIN_B=72
 MUX_GPIO_A=GPIO(MUX_PIN_A,"out")
 MUX_GPIO_B=GPIO(MUX_PIN_B,"out")
 
-##STEPPER MOTOR PINOUT
-DIRECTION_PIN=74
-STEPPER_PIN=73
-STEPPER_GPIO=GPIO(STEPPER_PIN,"out")
-DIRECTION_GPIO=GPIO(DIRECTION_PIN,"out")
-CW=True #Clockwise spining direction
-CCW=False #Counterclockwise spining direction
-speed=0.001
-height=1
-
 ##UART ENABLE / US100
 os.system("systemctl stop serial-getty@ttyS2.service")
-
-##UART LIDAR
-#UART_LIDAR=serial.Serial("/dev/ttyS4",baudrate=9600,timeout=2)
 
 def Sensors(VAR_A,VAR_B):     
     UART_US100=serial.Serial("/dev/ttyS4",baudrate=9600,timeout=1)
@@ -37,21 +24,6 @@ def Sensors(VAR_A,VAR_B):
     us100= adafruit_us100.US100(UART_US100)
     distance=us100.distance
     return(distance)
-
-def Stepper_motor(dir,speed,height):
-    DIRECTION_GPIO.write(dir)
-    for x in range (height*200):
-        try:
-            STEPPER_GPIO.write(True)
-            time.sleep(speed)
-            STEPPER_GPIO.write(False)
-            time.sleep(speed)
-        except KeyboardInterrupt:
-            STEPPER_GPIO.write(False)
-            break
-        except IOError:
-            print("IOErr")
-            break
 
 def main():
     while True:
