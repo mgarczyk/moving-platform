43e208736a002a5019cfeb92bac2fdf0b200f266
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-07-20 15:49:16 +0200

    Script that read data from us100 and send to broker.

--- us100_mux.py
+++ us100_mux.py
@@ -3,6 +3,7 @@ import adafruit_us100
 import mqtt_pub
 import time
 import serial
+import os
 
 ##MUX PINOUT##
 MUX_PIN_A=71
@@ -13,15 +14,15 @@ MUX_GPIO_B=GPIO(MUX_PIN_B,"out")
 ##BROKER##
 broker = 'localhost'
 port = 1883
-topic = "/home/radxa/moving_platform/mqtt/distance"
+topic = "/home/radxa/moving_platform/mqtt/us100"
 client_id = f'publish-distance'
 
 def Sensors(VAR_A,VAR_B):     
-    UART_US100=serial.Serial("/dev/ttyS4",baudrate=9600,timeout=1)
+    UART_US100=serial.Serial("/dev/ttyS4",baudrate=9600)
     MUX_GPIO_A.write(VAR_A)
     MUX_GPIO_B.write(VAR_B)
-    time.sleep(0.5)    
-    us100= adafruit_us100.US100(UART_US100)
+    time.sleep(0.001)    
+    us100=adafruit_us100.US100(UART_US100)
     distance=us100.distance
     return(distance)
 
@@ -31,16 +32,12 @@ def publisher_us100():
         try:
             x1=Sensors(False,False)
             mqtt_pub.publish(client, topic, f'x1: {x1}')
-            time.sleep(0.1)
             x2=Sensors(False,True)
             mqtt_pub.publish(client, topic, f'x2: {x2}')
-            time.sleep(0.1)
             x3=Sensors(True,False)
             mqtt_pub.publish(client, topic, f'x3: {x3}')
-            time.sleep(0.1)
             x4=Sensors(True,True)
             mqtt_pub.publish(client, topic, f'x4: {x4}')
-            time.sleep(0.1)
         except KeyboardInterrupt:
             MUX_GPIO_A.write(False)
             MUX_GPIO_B.write(False)
