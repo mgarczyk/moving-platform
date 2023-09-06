d40fc2425043c12c931e736e0fbd96260cc77ae7
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-09-06 14:55:29 +0200

    Add the flag that reset state of encoders.

--- left_enc.py
+++ left_enc.py
@@ -1,15 +1,23 @@
 import time
 import mqtt_pub
 from periphery import GPIO
+from paho.mqtt import client as mqtt_client
+import paho.mqtt.subscribe as subscribe
 
 ENCODER2_PIN_A = 76
 ENCODER2_PIN_B = 73
 DIR_L=71
 
+def on_message(client, obj, msg):
+    global reset_encoders
+    if int(msg.payload.decode('utf8')) == 1:
+        reset_encoders=1
+        print(1)
+    else:
+        reset_encoders=0
 
-def encoder_callback(pin_a, pin_b):
-    client = mqtt_pub.connect_mqtt("encoder_L", "localhost", 1883)
-    
+def encoder_callback(pin_a, pin_b, topic):
+    global reset_encoders
     tick_count = 0
     encoder_a = GPIO(pin_a, 'in')
     DIR = GPIO(DIR_L, 'in')
@@ -18,6 +26,11 @@ def encoder_callback(pin_a, pin_b):
     prev_value_a = encoder_a.read()
    
     while True:
+        if reset_encoders==1:
+            print(reset_encoders)
+            tick_count=0
+            reset_encoders=0
+            time.sleep(0.2)
         dir_state=DIR.read()
         a_state = encoder_a.read()
         b_state = encoder_b.read()
@@ -28,14 +41,25 @@ def encoder_callback(pin_a, pin_b):
                 tick_count-=1
             prev_value_a = a_state
             prev_b_state = b_state
-
             # Tutaj możesz umieścić swoje własne operacje lub wyświetlić wynik
-        mqtt_pub.publish(client, "mqtt/left_ticks", tick_count)
+        mqtt_pub.publish(client, topic_pub, tick_count)
         time.sleep(0.05)
 
 
 try:
-    encoder_callback(ENCODER2_PIN_A,ENCODER2_PIN_B)
+    broker = 'localhost'
+    port = 1883
+    topic_pub = "mqtt/left_ticks"
+    topic_sub = "mqtt/reset_encoders"
+    client = mqtt_client.Client()
+    client.connect(broker, port)
+    client.subscribe(topic_sub ,0)
+    client.on_message=on_message
+    client.loop_start()
+
+    reset_encoders=0
+
+    encoder_callback(ENCODER2_PIN_A,ENCODER2_PIN_B, topic_pub)
         
 except KeyboardInterrupt:
     pass
--- right_enc.py
+++ right_enc.py
@@ -1,16 +1,19 @@
 import time
 import mqtt_pub
 from periphery import GPIO
+from paho.mqtt import client as mqtt_client
+import paho.mqtt.subscribe as subscribe
 
 ENCODER1_PIN_A = 74
 ENCODER1_PIN_B = 158
 DIR_R= 72
 
+def on_message(client, obj, msg):
+    global reset_encoders
+    reset_encoders=1
 
-
-def encoder_callback(pin_a, pin_b):
-    client = mqtt_pub.connect_mqtt("encoder_R", "localhost", 1883)
-    
+def encoder_callback(pin_a, pin_b, topic):
+    global reset_encoders
     tick_count = 0
     encoder_a = GPIO(pin_a, 'in')
     DIR = GPIO(DIR_R, 'in')
@@ -19,6 +22,11 @@ def encoder_callback(pin_a, pin_b):
     prev_value_a = encoder_a.read()
    
     while True:
+        if reset_encoders==1:
+            print(reset_encoders)
+            tick_count=0
+            reset_encoders=0
+            time.sleep(0.2)
         dir_state=DIR.read()
         a_state = encoder_a.read()
         b_state = encoder_b.read()
@@ -29,14 +37,23 @@ def encoder_callback(pin_a, pin_b):
                 tick_count-=1
             prev_value_a = a_state
             prev_b_state = b_state
-
-            # Tutaj możesz umieścić swoje własne operacje lub wyświetlić wynik
-        mqtt_pub.publish(client, "mqtt/right_ticks", tick_count)
+        mqtt_pub.publish(client, topic_pub, tick_count)
         time.sleep(0.05)
 
-
 try:
-    encoder_callback(ENCODER1_PIN_A,ENCODER1_PIN_B)
+    broker = 'localhost'
+    port = 1883
+    topic_pub = "mqtt/right_ticks"
+    topic_sub = "mqtt/reset_encoders"
+    client = mqtt_client.Client()
+    client.connect(broker, port)
+    client.subscribe(topic_sub ,0)
+    client.on_message=on_message
+    client.loop_start()
+
+    reset_encoders=0
+
+    encoder_callback(ENCODER1_PIN_A,ENCODER1_PIN_B, topic_pub)
         
 except KeyboardInterrupt:
     pass
