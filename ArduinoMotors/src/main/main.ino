730aa1a6595f941e4fc935b65abe75573299a61e
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2023-09-22 15:46:27 +0200

    PWM and soft start changes.

--- ArduinoMotors/src/main/main.ino
+++ ArduinoMotors/src/main/main.ino
@@ -5,48 +5,65 @@
 #define DIR_LEFT 4
 #define PWM_L 5
 
+
+const int drift_correction = 5;
+const int PWM_step_delay = 100;
+const int PWM_val_turn = 150;
+const int PWM_max = 200;
+const int PWM_step_start = 20;
+const int PWM_step_stop = 20;
 String read_now = "";
 String read_before = "";
+
+
+void soft_start(){
+ for(int i=0; i<=PWM_max; i+=PWM_step_start){
+    analogWrite(PWM_R, i + drift_correction);
+    analogWrite(PWM_L, i);
+    delay(PWM_step_delay);
+  }
+}
+
+void soft_stop(){
+   for(int i=PWM_max; i>=1; i-=PWM_step_stop){
+    analogWrite(PWM_R, i);
+    analogWrite(PWM_L, i);
+    delay(PWM_step_delay);
+  }
+  analogWrite(PWM_L, 0);
+  analogWrite(PWM_R, 0);
+}
 
 void left() {
   digitalWrite(DIR_RIGHT,HIGH);
   digitalWrite(DIR_LEFT, LOW);
-  analogWrite(PWM_R, 120);
-  analogWrite(PWM_L, 120);
+  analogWrite(PWM_R, PWM_val_turn);
+  analogWrite(PWM_L, PWM_val_turn);
 }
 
 void right() {
   digitalWrite(DIR_RIGHT, LOW);
   digitalWrite(DIR_LEFT, HIGH);
-  analogWrite(PWM_R, 120);
-  analogWrite(PWM_L, 120);
+  analogWrite(PWM_R, PWM_val_turn);
+  analogWrite(PWM_L, PWM_val_turn);
 }
 
 void forward() {
   digitalWrite(DIR_RIGHT, LOW);
   digitalWrite(DIR_LEFT, LOW);
-  for(int i=0; i<=200; i+=20){
-    analogWrite(PWM_R, i);
-    analogWrite(PWM_L, i);
-    delay(25);
-  }
+  soft_start();
+ 
 }
 
 void back() {
   digitalWrite(DIR_RIGHT, HIGH);
   digitalWrite(DIR_LEFT, HIGH);
-  for(int i=0; i<=200; i+=20){
-    analogWrite(PWM_R, i);
-    analogWrite(PWM_L, i);
-    delay(25);
-  }
+  soft_start();
 }
 
 void stop_motors() {
-  digitalWrite(DIR_RIGHT, HIGH);
-  digitalWrite(DIR_LEFT, HIGH);
-  analogWrite(PWM_R, 0);
-  analogWrite(PWM_L, 0);
+  soft_stop();
+  
 }
 
 void choose_dir(String read_now) {
@@ -60,11 +77,11 @@ void choose_dir(String read_now) {
 
 void setup() {
   Serial.begin(9600);
-  Serial.flush()
+  Serial.flush();
   pinMode(DIR_LEFT, OUTPUT);
   pinMode(DIR_RIGHT, OUTPUT);
   pinMode(PWM_R, OUTPUT);
-  pinMode(PWM_L, OUTPUT)
+  pinMode(PWM_L, OUTPUT);
 }
 
 void loop() {
@@ -75,7 +92,5 @@ void loop() {
       choose_dir(read_now);
     }
     read_before = read_now;
-
-
   }
 }
--- auto.py
+++ auto.py
@@ -1,20 +1,12 @@
 import time
 import periphery
 import paho.mqtt.client as mqtt_client
-import os
 import math
 import logging
 import mqtt_pub
 import serial
-PWM_motor = periphery.PWM(0, 0)
-Dir_L_GPIO=periphery.GPIO(71,"out")
-Dir_R_GPIO=periphery.GPIO(72,"out")
 Dir_LIFT_GPIO=periphery.GPIO(157,"out")
 PWM_LIFT_GPIO=periphery.GPIO(42,"out")
-soft_start=True
-PWM_motor.enable()
-data = -1
-pomiar=True
 distance_tmp= 0 
 left_ticks=0
 right_ticks=0    
@@ -29,7 +21,7 @@ logging.basicConfig(filename='auto_logger.log', filemode='w', level=logging.DEBU
 
 def on_connect(client : mqtt_client, userdata, flags, rc):
     logging.info(f"Connected to broker with result code: {str(rc)}")
-    client.subscribe([("mqtt/left_ticks",1),("mqtt/right_ticks",1),("mqtt/lidar",0),("mqtt/distance",0)])
+    client.subscribe([("mqtt/left_ticks",1),("mqtt/right_ticks",1),("mqtt/lidar",0),("mqtt/us_100",0)])
 
 def remove_zeroes_LIDAR(vector : list):
     for elements in vector:
@@ -71,50 +63,21 @@ def on_message(client : mqtt_client, obj, msg):
         right_ticks = int(msg.payload.decode('utf8'))
     elif msg.topic=="mqtt/lidar":
         forw_dist, rig_dist, left_dist, reverse_dist = prepare_lidar_data(msg.payload)
-    elif msg.topic=="mqtt/distance":
+        print(forw_dist)
+    elif msg.topic=="mqtt/us_100":
         us_100 = prepare_us100_data(msg.payload)
-        
-def pwm_set_turn():
-       PWM_motor.duty_cycle = 0.5
 
-def pwm_set(soft_start : bool):
-    if soft_start == True:
-        for speed in [0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25]:
-            PWM_motor.duty_cycle = speed
-            time.sleep(0.07)
-    else:
-        PWM_motor.duty_cycle = 0.25
-    soft_start=False
-    return soft_start
+def forward():
+    arduino.write(b"forward\n")
 
-def forward(soft_start : bool):
-    ser.write(b"forward\n")
-    # Dir_L_GPIO.write(False)
-    # Dir_R_GPIO.write(False)
-    # soft_start=pwm_set(soft_start)
-     #return soft_start
-
-def back(soft_start : bool):
-    ser.write(b"back\n")
-
-    # Dir_L_GPIO.write(True)
-    # Dir_R_GPIO.write(True)
-    # soft_start=pwm_set(soft_start)
-    # return soft_start
+def back():
+    arduino.write(b"back\n")
 
 def left():
-        ser.write(b"left\n")
-
-    # Dir_L_GPIO.write(False)
-    # Dir_R_GPIO.write(True)
-    # pwm_set_turn()
+    arduino.write(b"left\n")
 
 def right():
-        ser.write(b"right\n")
-
-    # Dir_L_GPIO.write(True)
-    # Dir_R_GPIO.write(False)
-    # pwm_set_turn()
+    arduino.write(b"right\n")
 
 def lift():
     Dir_LIFT_GPIO.write(True)
@@ -125,22 +88,18 @@ def lower():
     PWM_LIFT_GPIO.write(True)
 
 def stop():
-        ser.write(b"stop\n")
-
-    # lower()
-    # PWM_motor.duty_cycle = 1.0
-    # return True
+    arduino.write(b"stop\n")
 
 def left_turn():
     left()
-    time.sleep(3.85)
+    time.sleep(4)
     stop()
     mqtt_pub.publish(client, "mqtt/reset_encoders", "1")
     time.sleep(0.1)
 
 def right_turn():
     right()
-    time.sleep(3.9)
+    time.sleep(4)
     stop()
     mqtt_pub.publish(client, "mqtt/reset_encoders", "1")
     time.sleep(0.1)
@@ -148,109 +107,108 @@ def right_turn():
 def alley_init(dist_beetwen_measures : float):
     mqtt_pub.publish(client, "mqtt/reset_encoders", "1")
     time.sleep(0.1)
-    global soft_start, distance_tmp
-    soft_start=True
+    global distance_tmp
     distance_tmp=0
     next_measure_pos=dist_beetween_measures
     sumaryczna_dlugosc_przeszkod = 0
-    return soft_start, distance_tmp, next_measure_pos, sumaryczna_dlugosc_przeszkod
+    forward()
+    return distance_tmp, next_measure_pos, sumaryczna_dlugosc_przeszkod
 
-def alley_drive( distance_tmp : float, sumaryczna_dlugosc_przeszkod : float):
+def alley_drive(distance_tmp : float, sumaryczna_dlugosc_przeszkod : float):
     encoder_tick=(left_ticks+right_ticks)/2
     distance_tmp = sumaryczna_dlugosc_przeszkod + (encoder_tick/14.85)*math.pi*0.102    
     print(distance_tmp)
-    forward(soft_start)
     return distance_tmp
 
-def alley_measure(distance_tmp : float, dist_beetween_measures : float, next_measure_pos : float,  soft_start : bool):
+def alley_measure(distance_tmp : float, dist_beetween_measures : float, next_measure_pos : float):
     logging.info(f"Mesure distance: {distance_tmp}")
-    soft_start=stop()
+    stop()
     lift()
     time.sleep(2)
     lower()
     time.sleep(2)
     next_measure_pos+=dist_beetween_measures
-    return next_measure_pos, soft_start
+    forward() # TODO przesunięte z drive
+    return next_measure_pos
 
 def alley_end():
     stop()
     time.sleep(0.5)
 
 def alley(distance_to_travel : float, dist_beetween_measures : float, is_measure : bool):
-    soft_start, distance_tmp, next_measure_pos, sumaryczna_dlugosc_przeszkod = alley_init(dist_beetween_measures)
+    distance_tmp, next_measure_pos, sumaryczna_dlugosc_przeszkod = alley_init(dist_beetween_measures)
     while distance_tmp<distance_to_travel:
-    #     #AVOIDING THE OBSTACLE#
-    #     if any(x<60 for x in forw_dist) or any(y<30 for y in us_100):
-    #         szerokosc_przeszkody = 0
-    #         szerokosc_przeszkody_powrot = 0
-    #         dlugosc_przeszkody = 0
-    #         if us_100[2]<30 or us_100[3]<30 :
-    #             logging.info(f"Avoiding obstacle from right side.")
-    #             left_turn()  # skret w lewo po wykryciu przeszkody
-    #             while any(x<80 for x in rig_dist): # jazda prosto do konca przeszkody - wolnego miejsca po prawej 
-    #                 if any(x<50 for x in us_100):
-    #                     logging.info("Can't avoid STOP.")
-    #                     stop()
-    #                     exit()
-    #                 encoder_tick=(left_ticks+right_ticks)/2
-    #                 szerokosc_przeszkody=(encoder_tick/15)*math.pi*0.102 # droga pokonana na szerokosc
-    #                 soft_start=forward(soft_start) # jazda do przodu do momentu spelnienia warunku
-    #             logging.info(f"Obstacle width: {szerokosc_przeszkody}")
-    #             right_turn() # koniec przeszkody 
-    #             while any(x<80 for x in rig_dist): # jazda na długosc 
-    #                 #print(forw_dist)
-    #                 if any(x<50 for x in us_100):
-    #                     logging.error("Can't avoid STOP.")
-    #                     stop()
-    #                     exit()
-    #                 encoder_tick=(left_ticks+right_ticks)/2
-    #                 dlugosc_przeszkody=(encoder_tick/15)*math.pi*0.102
-    #                 soft_start=forward(soft_start)
-    #             sumaryczna_dlugosc_przeszkod += dlugosc_przeszkody
-    #             logging.info(f"Obstacle length: {dlugosc_przeszkody}")
-    #             right_turn() # powrot na srodek sciezki
-    #             while szerokosc_przeszkody_powrot<=szerokosc_przeszkody:
-    #                 encoder_tick=(left_ticks+right_ticks)/2
-    #                 szerokosc_przeszkody_powrot=(encoder_tick/15)*math.pi*0.102
-    #                 soft_start=forward(soft_start)
-    #             left_turn()
-    #             logging.info(f"Obstacle avoided.")
-    #         else:
-    #             logging.warning(f"Avoiding obstacle from left side.")
-    #             right_turn()
-    #             while any(x<80 for x in left_dist): # jazda prosto do konca przeszkody - wolnego miejsca po prawej 
-    #                 if any(x<50 for x in us_100):
-    #                     logging.error("Can't avoid STOP.")
-    #                     stop()
-    #                     exit()
-    #                 encoder_tick=(left_ticks+right_ticks)/2
-    #                 szerokosc_przeszkody=(encoder_tick/15)*math.pi*0.102 # droga pokonana na szerokosc 
-    #                 soft_start=forward(soft_start) # jazda do przodu do momentu spelnienia warunku
-    #             logging.info(f"Obstacle width: {szerokosc_przeszkody}")
-    #             left_turn() # koniec przeszkody 
-    #             while any(x<80 for x in left_dist): # jazda na długosc 
-    #                 #print(forw_dist)
-    #                 if any(x<50 for x in us_100):
-    #                     logging.error("Can't avoid STOP.")
-    #                     stop()
-    #                     exit()
-    #                 encoder_tick=(left_ticks+right_ticks)/2
-    #                 dlugosc_przeszkody=(encoder_tick/15)*math.pi*0.102
-    #                 soft_start=forward(soft_start)
-    #             logging.info(f"Obstacle length: {dlugosc_przeszkody}")
-    #             sumaryczna_dlugosc_przeszkod += dlugosc_przeszkody
-    #             left_turn() # powrot na srodek sciezki
-    #             while szerokosc_przeszkody_powrot<=szerokosc_przeszkody:
-    #                 encoder_tick=(left_ticks+right_ticks)/2
-    #                 szerokosc_przeszkody_powrot=(encoder_tick/15)*math.pi*0.102
-    #                 soft_start=forward(soft_start)
-    #             right_turn()
-    #             logging.info(f"Obstacle avoided.")
-    #     ##DRIVING WHEN THERE ARE NO OBSTACLES##
-    #     else:
-        distance_tmp = alley_drive(distance_tmp, sumaryczna_dlugosc_przeszkod)
-        if distance_tmp>=next_measure_pos and is_measure==True:
-                next_measure_pos, soft_start = alley_measure(distance_tmp, dist_beetween_measures, next_measure_pos, soft_start)
+        #AVOIDING THE OBSTACLE#
+        if any(x<60 for x in forw_dist) or any(y<30 for y in us_100):
+            szerokosc_przeszkody = 0
+            szerokosc_przeszkody_powrot = 0
+            dlugosc_przeszkody = 0
+            if us_100[2]<30 or us_100[3]<30 :
+                logging.info(f"Avoiding obstacle from right side.")
+                left_turn()  # skret w lewo po wykryciu przeszkody
+                while any(x<80 for x in rig_dist): # jazda prosto do konca przeszkody - wolnego miejsca po prawej 
+                    if any(x<50 for x in us_100):
+                        logging.info("Can't avoid STOP.")
+                        stop()
+                        exit()
+                    encoder_tick=(left_ticks+right_ticks)/2
+                    szerokosc_przeszkody=(encoder_tick/15)*math.pi*0.102 # droga pokonana na szerokosc
+                    forward() # jazda do przodu do momentu spelnienia warunku
+                logging.info(f"Obstacle width: {szerokosc_przeszkody}")
+                right_turn() # koniec przeszkody 
+                while any(x<80 for x in rig_dist): # jazda na długosc 
+                    if any(x<50 for x in us_100):
+                        logging.error("Can't avoid STOP.")
+                        stop()
+                        exit()
+                    encoder_tick=(left_ticks+right_ticks)/2
+                    dlugosc_przeszkody=(encoder_tick/15)*math.pi*0.102
+                    forward()
+                sumaryczna_dlugosc_przeszkod += dlugosc_przeszkody
+                logging.info(f"Obstacle length: {dlugosc_przeszkody}")
+                right_turn() # powrot na srodek sciezki
+                while szerokosc_przeszkody_powrot<=szerokosc_przeszkody:
+                    encoder_tick=(left_ticks+right_ticks)/2
+                    szerokosc_przeszkody_powrot=(encoder_tick/15)*math.pi*0.102
+                    forward()
+                left_turn()
+                logging.info(f"Obstacle avoided.")
+            else:
+                logging.warning(f"Avoiding obstacle from left side.")
+                right_turn()
+                while any(x<80 for x in left_dist): # jazda prosto do konca przeszkody - wolnego miejsca po prawej 
+                    if any(x<50 for x in us_100):
+                        logging.error("Can't avoid STOP.")
+                        stop()
+                        exit()
+                    encoder_tick=(left_ticks+right_ticks)/2
+                    szerokosc_przeszkody=(encoder_tick/15)*math.pi*0.102 # droga pokonana na szerokosc 
+                    forward() # jazda do przodu do momentu spelnienia warunku
+                logging.info(f"Obstacle width: {szerokosc_przeszkody}")
+                left_turn() # koniec przeszkody 
+                while any(x<80 for x in left_dist): # jazda na długosc 
+                    #print(forw_dist)
+                    if any(x<50 for x in us_100):
+                        logging.error("Can't avoid STOP.")
+                        stop()
+                        exit()
+                    encoder_tick=(left_ticks+right_ticks)/2
+                    dlugosc_przeszkody=(encoder_tick/15)*math.pi*0.102
+                    forward()
+                logging.info(f"Obstacle length: {dlugosc_przeszkody}")
+                sumaryczna_dlugosc_przeszkod += dlugosc_przeszkody
+                left_turn() # powrot na srodek sciezki
+                while szerokosc_przeszkody_powrot<=szerokosc_przeszkody:
+                    encoder_tick=(left_ticks+right_ticks)/2
+                    szerokosc_przeszkody_powrot=(encoder_tick/15)*math.pi*0.102
+                    forward()
+                right_turn()
+                logging.info(f"Obstacle avoided.")
+        ##DRIVING WHEN THERE ARE NO OBSTACLES##
+        else:
+            distance_tmp = alley_drive(distance_tmp, sumaryczna_dlugosc_przeszkod)
+            if distance_tmp>=next_measure_pos and is_measure==True:
+                    next_measure_pos = alley_measure(distance_tmp, dist_beetween_measures, next_measure_pos)
                     
 def curve(route : str, shelf_width : float,alley_width:float):
     mqtt_pub.publish(client, "mqtt/reset_encoders", "1")
@@ -310,8 +268,8 @@ def user_input():
     return length_of_alley, shelf_width, number_of_shelfs, route, dist_beetween_measures, alley_width
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
+    arduino = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+    arduino.reset_input_buffer()
     logging.info("====================")
     logging.info('-----New sesion-----')
     logging.info("====================")  
@@ -328,10 +286,8 @@ if __name__ == '__main__':
         try:
                 mqtt_pub.publish(client, "mqtt/reset_encoders", "1")
                 time.sleep(0.3)
-                alley(length_of_alley, dist_beetween_measures, True)
-                stop()
-                soft_start=True
                 lift_flag=True
+                alley(length_of_alley, dist_beetween_measures, lift_flag)
                 actual_shelf=actual_shelf+1
                 if actual_shelf<=number_of_shelfs:
                     route=curve(route,shelf_width,alley_width) 
@@ -340,4 +296,3 @@ if __name__ == '__main__':
             logging.info('-----Manual exit-----')
             exit()
     stop() 
-    distance_tmp=0
